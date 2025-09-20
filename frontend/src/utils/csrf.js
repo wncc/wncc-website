c95a46b1e964cf9e1e1e40422a7f9f435ca9713f
Pratyaksh Bhardwaj <pratyakshb349@gmail.com>
2025-09-20 16:13:52 +0530

    csrf issue

--- frontend/src/utils/csrf.js
+++ frontend/src/utils/csrf.js
@@ -18,7 +18,7 @@ export const getCSRFToken = () => {
 // Fetch CSRF token from Django
 export const fetchCSRFToken = async () => {
     try {
-        const response = await fetch(`https://wnccb.tech-iitb.org/api/csrf/`, {
+        const response = await fetch(`https://wnccb.tech-iitb.org/api/auth/csrf/`, {
             credentials: 'include'
         });
         const data = await response.json();
